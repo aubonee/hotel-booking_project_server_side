694b90bf1899f48ab7352b512ec3a83145bb8179
aubonee <chytahsin.2210@gmail.com>
2023-11-06 21:15:12 +0600

    rooms data get done

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express();
-
-
 
 const port =process.env.PORT || 5000;
 
@@ -12,16 +10,11 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 //hoteldb_user
 //i2i9qHxX2RwdKVoW
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jkbvpzz.mongodb.net/?retryWrites=true&w=majority`;
+//const uri = `mongodb+srv://hoteldb_user:i2i9qHxX2RwdKVoW@cluster0.jkbvpzz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,12 +29,43 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const roomsCollection =client.db('hoteldb').collection('roomsCollection');
+//get all rooms
+
+    app.get('/rooms',async(req,res)=>{
+        const cursor =roomsCollection.find();
+        const result =await cursor.toArray();
+        res.send(result);
+    })
+
+
+    //room detail data get
+    app.get('/rooms/:id', async (req, res) => {
+        const id  = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+    //     const options = {
+          
+    //         projection: { description: 1, room_image:1, description:1, room_size: 1 },
+    //     };
+    //    const result = await roomsCollection.findOne(query,options);
+      const result = await roomsCollection.findOne(query);
+        res.send(result);
+        
+      })
+
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+  } 
+  catch (error) {
+    console.error('An error occurred:', error);
+  }
+  finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
