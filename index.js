ff54cdc7b68ec218d546addf7cdcea5a6293f486
aubonee <chytahsin.2210@gmail.com>
2024-01-06 01:38:00 +0600

    upadate and delete api added

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ const port =process.env.PORT || 5000;
 
 //middleware
 app.use(cors({
- // origin: ['http://localhost:5173'],
-  origin: [
-    'https://hotel-service-5e6d3.firebaseapp.com/',
-    'https://hotel-service-5e6d3.web.app/'],
+  origin: [ 'http://localhost:5173',
+  'http://localhost:5174',
+  'https://ossified-mark.surge.sh',
+    'https://hotel-service-5e6d3.firebaseapp.com',
+    'https://hotel-service-5e6d3.web.app'],
   credentials: true
 
 }));
@@ -26,7 +27,6 @@ app.use(cookieParser());
 //abcd1234
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jkbvpzz.mongodb.net/?retryWrites=true&w=majority`;
-//const uri = `mongodb+srv://hoteldb_user:i2i9qHxX2RwdKVoW@cluster0.jkbvpzz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -66,6 +66,7 @@ async function run() {
 
     const roomsCollection =client.db('hoteldb').collection('roomsCollection');
     const bookingsCollection =client.db('hoteldb').collection('bookingsCollection');
+    const userCollection =client.db('hoteldb').collection('users');
 //////////auth related api
 
 app.post('/jwt',async(req,res)=>{
@@ -74,7 +75,8 @@ app.post('/jwt',async(req,res)=>{
   const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
  console.log(token);
   res.cookie('token',token,{
-    httpOnly : true,  secure: false, }).send({success:true})
+    httpOnly : true,  secure: process.env.NODE_ENV === 'production', 
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict' }).send({success:true})
 })
 ////////server related Api
     //get all rooms
@@ -109,33 +111,51 @@ app.post('/jwt',async(req,res)=>{
     delete newbooking._id;
     console.log(newbooking);
     const result =await bookingsCollection.insertOne(newbooking);
-
-
-    // if (room.quantity > 0) {
-   
-    //   roomsCollection.quantity -= 1;
-    //   await room.save();
-    // }
    
     res.send(result);
 
   })
 
-  app.get('/bookings/:email', logger, verifyToken, async (req, res) => {
+  app.get('/bookings/:email',  async (req, res) => {
     const email  = req.params.email;
-    console.log('token:', req.cookies.token)
-  console.log('user in the valid token:', req.user)
-   
-  //  if(req.query.email !== req.user.email){
-  //   return res.status(403).send({message:'forbidden access'})
-  //  }
-    let query = { email }
 
-    
+  console.log('user in the valid token:', req.user)
+    let query = { email }
     const result = await bookingsCollection.find(query).toArray();
     res.send(result);
     
   })
+
+
+  app.get('/updateBooking',async(req,res)=>{
+    const cursor =bookingsCollection.find();
+    const result =await cursor.toArray();
+    res.send(result);
+})
+//////////////////////update
+  app.get('/updateBooking/:id', async (req, res) => {
+    const id  = req.params.id;
+    const query = { _id: new ObjectId(id) }
+  const result = await bookingsCollection.findOne(query);
+    res.send(result);
+    
+  })
+
+  app.put('/updateBooking/:id', async(req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)}
+    const options = { upsert: true };
+    const updatedbooking = req.body;
+
+    const booking = {
+        $set: {
+          date: updatedbooking.date
+        }
+    }
+
+    const result = await bookingsCollection.updateOne(filter, booking, options);
+    res.send(result);
+})
 ///booking data delete
   app.delete('/bookings/:id', async (req, res) => {
     const id = req.params.id;
@@ -143,6 +163,11 @@ app.post('/jwt',async(req,res)=>{
     const result = await bookingsCollection.deleteOne(query);
     res.send(result);
 })
+// app.post('/users', async(req,res)=>{
+//   const user =req.body;
+//   const result =await userCollection.insertOne(user);
+//   res.send(result);
+// })
 ///////////////////////////////////
 
    
